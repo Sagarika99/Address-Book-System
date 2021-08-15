5a6311eced101bec172df40c768cff623785cef5
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-16 00:54:49 +0530

    First Commit

---
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.addressbook;
+
+class contactBook{
+	String firstName;
+	String lastName;
+	String address;
+	String state;
+	String city;
+	int zip;
+	long phoneNum;
+
+	public contactBook (String firstName, String lastName, String address, String state, String city, int zip, long phoneNum){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.state = state;
+		this.city = city;
+		this.zip = zip;
+		this.phoneNum = phoneNum;
+	}
+}
+
+
+public class AddressBook {
+	public static void main(String[] args) {
+		
+	}
+}
