16de3ef7c5879fb1627a170b944dc7e0d72d1744
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-08-17 14:17:41 +0530

    Edit contacts features added

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbook;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 class contactBook{
@@ -21,39 +22,106 @@ class contactBook{
 		this.zip = zip;
 		this.phoneNum = phoneNum;
 	}
-	
-    public void PrintPersonDetails(){
-        System.out.println(firstName);
-        System.out.println(lastName);
-        System.out.println(address);
-        System.out.println(city);
-        System.out.println(state);
-        System.out.println(zip);
-        System.out.println(phoneNum);
-    }
-   
 }
 
+public class AddressBook{
+	Scanner sc = new Scanner(System.in);
+	int record = 0;
+	public contactBook details[];
 
-public class AddressBook {
+    public void addContact() {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter number of entries: ");
+    	int record = sc.nextInt();
+    	details = new contactBook[record];
+    	for (int i=0; i<record; i++) {
+    		System.out.println(i+1);
+    		System.out.println("Enter First name: ");
+        	String firstName = sc.next();
+        	System.out.println("Enter Last name: ");
+        	String lastName = sc.next();
+        	System.out.println("Enter address: ");
+        	String address = sc.next();
+        	System.out.println("Enter city: ");
+        	String city = sc.next();
+        	System.out.println("Enter state: ");
+        	String state = sc.next();
+        	System.out.println("Enter zip code: ");
+        	int zip = sc.nextInt();
+        	System.out.println("Enter phone number: ");
+        	String phoneNum = sc.next();
+        	
+        	details[i] = new contactBook (firstName, lastName, address, city, state, zip, phoneNum );
+        	//book.PrintPersonDetails();
+    	}
+}
+    	
+    public void PrintPersonDetails(){
+      	for (int i=0; i < details.length; i++) {
+      		if(details[i]!=null) {
+      		  System.out.println("First Name: "+details[i].firstName);
+              System.out.println("Last Name: "+details[i].lastName);
+              System.out.println("Address: "+details[i].address);
+              System.out.println("City: "+details[i].city);
+              System.out.println("State: "+details[i].state);
+              System.out.println("Zip Code: "+details[i].zip);
+              System.out.println("Phone Number: "+details[i].phoneNum);
+              System.out.println();
+      		}
+      	}
+      }
+      
+    public void editDetails() {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter the First Name: ");
+    	String name = sc.next();
+    	for (int j=0; j < details.length; j++) {
+        	if(name.equals(details[j].firstName)){
+                System.out.println("Enter First name");
+                details[j].firstName = sc.next();
+                System.out.println("Enter Last name");
+                details[j].lastName = sc.next();
+                System.out.println("Enter address");
+                details[j].address = sc.next();
+                System.out.println("Enter city");
+                details[j].city = sc.next();
+                System.out.println("Enter state");
+                details[j].state = sc.next();
+                System.out.println("Enter zip code");
+                details[j].zip = sc.nextInt();
+                System.out.println("Enter phone number");
+                details[j].phoneNum = sc.next();
+        	}
+    	}
+
+    	
+    }
+
 	public static void main(String[] args) {
+		AddressBook bookBuilder = new AddressBook();
 		Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First name");
-        String firstName = sc.nextLine();
-        System.out.println("Enter Last name");
-        String lastName = sc.nextLine();
-        System.out.println("Enter address");
-        String address = sc.nextLine();
-        System.out.println("Enter city");
-        String city = sc.nextLine();
-        System.out.println("Enter state");
-        String state = sc.nextLine();
-        System.out.println("Enter zip code");
-        int zip = sc.nextInt();
-        System.out.println("Enter phone number");
-        String phoneNum = sc.nextLine();
-        
-        contactBook book = new contactBook (firstName, lastName, address, city, state, zip, phoneNum );
-        book.PrintPersonDetails();
+		int flag=0;
+		while (flag==0) {
+			System.out.println("Choose an option: ");
+			System.out.println("1. Add a contact ; 2. Edit a contact ; 3. Delete a contact ; 4. Print all contacts 5. Exit: ");
+			int i = sc.nextInt();
+			//int total = 0;
+			switch(i) {
+				case 1:
+					bookBuilder.addContact();
+					break;
+				case 2:
+					bookBuilder.editDetails();
+					break;
+				case 3:
+					
+				case 4:
+					bookBuilder.PrintPersonDetails();
+					break;
+				case 5:
+					flag = 1;
+					break;
+			}
+		}
 	}
 }
